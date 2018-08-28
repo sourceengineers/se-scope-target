32b1c2c2dd5b66d58d367876c69307d3a9509299
rockresu <schuepbs@gmail.com>
2018-08-28 16:59:10 +0200

    Added Project structure

---
+++ libs/msgpack-parser/src/msgpack-parser.c
@@ -0,0 +1,1 @@
+#include <msgpack-parser/msgpack-parser.h>
---
+++ src/main.c
@@ -0,0 +1,12 @@
+
+#include <msgpack-parser/msgpack-parser.h>
+#include <stdio.h>
+
+int main()
+{
+
+    printf("%s\n", "Hallo Welt");
+
+
+    return 0;
+}
