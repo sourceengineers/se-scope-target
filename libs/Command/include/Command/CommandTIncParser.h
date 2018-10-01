c2bea7cb54d11d22e4cadd2b582439e52fb2e952
rockresu <schuepbs@gmail.com>
2018-10-01 17:42:21 +0300

    Added cf_t_inc parser

---
+++ libs/Command/include/Command/CommandTIncParser.h
@@ -0,0 +1,36 @@
+/*!*****************************************************************************
+ * @file         CommandTInc.h
+ *
+ * @copyright    Copyright (c) 2018 by Sourceengineers. All Rights Reserved.
+ *
+ * @authors      Samuel Schuepbach samuel.schuepbach@sourceengineers.com
+ *
+ * @brief        Implementation of the configurator for the cf_TInc.
+ *
+ *              
+ ******************************************************************************/
+ 
+#ifndef COMMANDTINCPARSER_H_
+#define COMMANDTINCPARSER_H_
+
+#include <Command/ICommand.h>
+#include <Communication/Unpacker.h>
+#include <Command/CommandTInc.h>
+
+
+/* Defines class */
+typedef struct __CommandTIncParserPrivateData* CommandTIncParserHandle;
+
+/******************************************************************************
+ Public functions 
+******************************************************************************/
+/* Constructor: Creates a new instanze of the channel */
+CommandTIncParserHandle CommandTIncParser_create(ICommandHandle iCommand, IUnpackerHandle iUnpacker);
+
+/* Configurates the gives channel */
+void CommandTIncParser_configure(CommandTIncParserHandle self);
+
+/* Deconstructor: Deletes the instanze of the channel */
+void CommandTIncParser_destroy(CommandTIncParserHandle self);
+
+#endif
---
+++ libs/Command/src/CommandTIncParser.c
@@ -0,0 +1,43 @@
+/*!****************************************************************************************************************************************
+ * @file         CommandTIncParser.c
+ *
+ * @copyright    Copyright (c) 2018 by Sourceengineers. All Rights Reserved.
+ *
+ * @authors      Samuel Schuepbach samuel.schuepbach@sourceengineers.com
+ *
+ *****************************************************************************************************************************************/
+
+#include <Command/CommandTIncParser.h>
+
+
+typedef struct __CommandTIncParserPrivateData
+{
+  ICommandHandle iCommand;
+  IUnpackerHandle iUnpacker;
+  char* commandName;
+
+} CommandTIncParserPrivateData ;
+
+
+CommandTIncParserHandle CommandTIncParser_create(ICommandHandle iCommand, IUnpackerHandle iUnpacker){
+  CommandTIncParserHandle self = malloc(sizeof(CommandTIncParserPrivateData));
+  self->iCommand = iCommand;
+  self->iUnpacker = iUnpacker;
+  self->commandName = self->iCommand->getCommandName(self->iCommand);
+
+  return self;
+}
+
+void CommandTIncParser_configure(CommandTIncParserHandle self){
+
+  const int timeIncrement = self->iUnpacker->getIntFromCommand(self->iUnpacker, (const char*) self->commandName,
+                                                        (const char*) "");
+
+  self->iCommand->setCommandAttribute(self->iCommand, (void*) &timeIncrement);
+}
+
+/* Deconstructor: Deletes the instanze of the channel */
+void CommandTIncParser_destroy(CommandTIncParserHandle self){
+  free(self);
+  self = NULL;
+}
