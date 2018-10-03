78b528faa9d5036d41c616b68c10e8de03581291
rockresu <schuepbs@gmail.com>
2018-10-03 21:47:48 +0300

    Added new communications types file

---
+++ libs/GeneralPurpose/include/GeneralPurpose/DataTypes.h
@@ -0,0 +1,20 @@
+/*!****************************************************************************************************************************************
+ * @file         Types.h
+ *
+ * @copyright    Copyright (c) 2018 by Sourceengineers. All Rights Reserved.
+ *
+ * @authors      Samuel Schuepbach samuel.schuepbach@sourceengineers.com
+ *
+ * @brief        Defines different types in a enum to achieve a "template" kind
+ *               of programming.
+ *
+ *****************************************************************************************************************************************/
+ 
+#ifndef TYPES_H_
+#define TYPES_H_
+
+
+typedef enum {UINT8, UINT16, UINT32, UINT64, FLOAT, DOUBLE} DATA_TYPES;
+
+
+#endif
