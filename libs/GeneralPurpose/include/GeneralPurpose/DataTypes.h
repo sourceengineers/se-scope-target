81d61f0a1d4d6aae35608a2c6d8d5c00fb23d6e4
rockresu <schuepbs@gmail.com>
2018-10-20 09:34:30 +0300

    Added names of datatypes

--- libs/GeneralPurpose/include/GeneralPurpose/DataTypes.h
+++ libs/GeneralPurpose/include/GeneralPurpose/DataTypes.h
@@ -13,6 +13,11 @@
 #ifndef DATATYPES_H_
 #define DATATYPES_H_
 
+static const char* UINT8_NAME = "UINT8";
+static const char* UINT16_NAME = "UINT16";
+static const char* UINT32_NAME = "UINT32";
+static const char* FLOAT_NAME = "FLOAT";
+
 typedef enum {UINT8, UINT16, UINT32, UINT64, FLOAT, DOUBLE} DATA_TYPES;
 
 
