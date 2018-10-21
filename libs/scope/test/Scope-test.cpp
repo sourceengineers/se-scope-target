13287b845389cb2b9886c20e9e95e3e8f82779f8
rockresu <schuepbs@gmail.com>
2018-10-21 12:12:16 +0300

    Test multiple commands

--- libs/scope/test/Scope-test.cpp
+++ libs/scope/test/Scope-test.cpp
@@ -52,6 +52,37 @@ const char partThree[] = "\xa6\x31\x5f\x74\x79\x70\x65\xa5\x46\x4c\x4f\x41\x54\x
 
 const char intPrefix[] = "\xce";
 
+
+/* Msgpart 1 */
+/*
+{
+	"transport": null,
+	"payload": {
+		"sc_cmd": {
+			"ev_poll": 100
+		}
+	}
+}
+
+{
+	"transport": null,
+	"payload": {
+		"sc_cmd": {
+			"ev_trans": ""
+		}
+	}
+}
+*/
+
+const uint8_t* ev_poll = (const uint8_t*) "\x82\xa9\x74\x72\x61\x6e\x73\x70\x6f\x72\x74\xc0\xa7\x70\x61\x79\x6c\x6f\x61\x64\x81\xa6\x73\x63\x5f\x63\x6d\x64\x81\xa7\x65\x76\x5f\x70\x6f\x6c\x6c\x64";
+const size_t ev_pollLength = 38;
+
+const uint8_t* ev_trans = (const uint8_t*) "\x82\xa9\x74\x72\x61\x6e\x73\x70\x6f\x72\x74\xc0\xa7\x70\x61\x79\x6c\x6f\x61\x64\x81\xa6\x73\x63\x5f\x63\x6d\x64\x81\xa8\x65\x76\x5f\x74\x72\x61\x6e\x73\xa0";
+const size_t ev_transLength = 39;
+
+const uint8_t* faultyMessage = (const uint8_t*) "\x72\x61\x6e\x73\x70\x6f\x72\x74\xc0\xa7\x70\x61\x79\x6c\x6f\x61\x64\x81\xa6\x73\x63\x5f\x63\x6d\x64\x81\xa8\x65\x76\x5f\x74\x72\x61\x6e\x73\xa0";
+const size_t faultyMessageLength = 36;
+
 void copyByte(char* data, void* addr){
   const char* tmp =  (char*) addr;
   for(int i = 0; i<4; i++){
@@ -59,6 +90,10 @@ void copyByte(char* data, void* addr){
   }
   data[4] = '\0';
 }
+
+
+
+
 
 void transmit(IByteStreamHandle stream){
 
@@ -77,8 +112,8 @@ TEST(Scope, test_all)
 
   const size_t maxLength = 230;
 
-  const float testDataFloat[5] = {1.1f,2.2f,3.3f,4.4f,5.5f};
-  const uint32_t testDataInt[5] = {10,11,12,13,14};
+  const float testDataFloat[7] = {1.1f,2.2f,3.3f,4.4f,5.5f,6.6f,7.7f};
+  const uint32_t testDataInt[7] = {10,11,12,13,14,15,16};
 
   volatile float testVarFloat;
   volatile uint32_t testVarInt;
@@ -113,7 +148,7 @@ TEST(Scope, test_all)
 
   data[len] = '\0';
 
-  ScopeHandle scope = Scope_create(100,2, UART, TIMESTAMP_MANUAL, transmit);
+  ScopeHandle scope = Scope_create(100,2, ETHERNET, TIMESTAMP_MANUAL, transmit);
 
   IByteStreamHandle inputStream = Scope_getInputStream(scope);
 
@@ -129,5 +164,25 @@ TEST(Scope, test_all)
   }
 
   Scope_packMessage(scope);
+
+
+  /* Test ev_poll twice */
+  testVarFloat = testDataFloat[5];
+  testVarInt = testDataInt[5];
+  inputStream->write(inputStream, ev_poll, ev_pollLength);
+  Scope_command(scope);
+
+  testVarFloat = testDataFloat[6];
+  testVarInt = testDataInt[6];
+  inputStream->write(inputStream, ev_poll, ev_pollLength);
+  Scope_command(scope);
+
+  /* Test ev_trans */
+  inputStream->write(inputStream, ev_trans, ev_transLength);
+  Scope_command(scope);
+
+  /* Test faulty message */
+  inputStream->write(inputStream, faultyMessage, faultyMessageLength);
+  Scope_command(scope);
 
 }
