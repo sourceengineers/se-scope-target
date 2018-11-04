5452afefdde191bf7f1ba39f2d6f45698b429933
rockresu <schuepbs@gmail.com>
2018-11-04 13:23:43 +0200

    Fixed trigger test

--- test/Command/CommandTrigger-test.cpp
+++ test/Command/CommandTrigger-test.cpp
@@ -15,8 +15,8 @@ TEST(CommandTrigger, test_command)
   TriggerHandle trigger = Trigger_create();
   TriggerConfiguration conf = {.level = 4.4f, .edge = TRIGGER_EDGE_POSITIVE, .mode = TRIGGER_NORMAL, .stream = stream, .channelId = 0};
 
-  stream.writeData(&stream, 5.5f);
   stream.writeData(&stream, 1.1f);
+  stream.writeData(&stream, 5.5f);
 
   CommandTriggerHandle commandTrigger = CommandTrigger_create(trigger);
   ICommand* iCommand = CommandTrigger_getICommand(commandTrigger);
