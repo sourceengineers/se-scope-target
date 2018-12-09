9f7cf9cf3b84418a2b5fb617899ecdd174e0b616
rockresu <schuepbs@gmail.com>
2018-12-09 11:52:02 +0200

    Updated tool to python3

--- tools/dev/data_printer.py
+++ tools/dev/data_printer.py
@@ -4,12 +4,9 @@ import sys
 import json
 import matplotlib.pyplot as plt
 
-json_data_1 = '{"transport":"...","payload":{"sc_data":{"cl_data":{"cl_id_0":[1.5,345,274,124.9],"cl_id_1":[2304,9183.1,3492.1,3894.1]},"sc_announce":{"var_name_0":["addr_0","addr_type_0"],"var_name_1":["addr_1","addr_type_1"],"var_name_2":["addr_2","addr_type_2"],"cl_amount":100},"t_stmp":[0,10,20,25],"t_inc":"Increment between values","tgr":{"found":"True/False (always false if Continous)","cl_id":"Channel Id","cl_data_ind":"Index on which value was triggered"}}}}'
-json_data_2 = '{"transport":"...","payload":{"sc_data":{"cl_data":{"cl_id_0":[324,12,342,5234],"cl_id_1":[11111,213,36456,123]},"sc_announce":{"var_name_0":["addr_0","addr_type_0"],"var_name_1":["addr_1","addr_type_1"],"var_name_2":["addr_2","addr_type_2"],"cl_amount":100},"t_stmp":[30,40,55,60],"t_inc":"Increment between values","tgr":{"found":"True/False (always false if Continous)","cl_id":"Channel Id","cl_data_ind":"Index on which value was triggered"}}}}'
-
 class DataPrinter:
-    
-    def __init__(self, length):    
+
+    def __init__(self, length):
         self.maxlen = length;
         self.data_collection = collections.ChainMap()
         self.timestamp = collections.deque(maxlen=self.maxlen)
@@ -20,6 +17,9 @@ class DataPrinter:
     def plot_data(self, json_data):
         data = json.loads(json_data)
 
+        if ("cl_data" in data["payload"]["sc_data"]) == False:
+                    return;
+
         timestamp_tmp = data["payload"]["sc_data"]["t_stmp"];
         for x in range(0, len(timestamp_tmp)):
             self.timestamp.append(timestamp_tmp[x]);
@@ -28,14 +28,15 @@ class DataPrinter:
         for x in range(0, len(channels)):
             channel_name = list(channels.keys())[x]
             channel_data = channels[channel_name];
-            
+
             if ((channel_name in self.data_collection) == False):
                 print("Creating new map entry");
                 self.data_collection[channel_name] = collections.deque(maxlen=self.maxlen);
-            
+
             for y in range(0, len(channel_data)):
                 self.data_collection[channel_name].append(channel_data[y]);
-                
-            self.ax.plot(list(self.timestamp), list(self.data_collection[channel_name]), '-')
+
+            if (len(list(self.timestamp)) == len(list(self.data_collection[channel_name]))):
+                self.ax.plot(list(self.timestamp), list(self.data_collection[channel_name]), '-')
 
         self.f.canvas.draw()
--- tools/dev/uart_msgpack_tester.py
+++ tools/dev/uart_msgpack_tester.py
@@ -4,30 +4,43 @@ import os
 import sys
 import msgpack
 from data_printer import DataPrinter
-
+import json
+import collections
+from six import string_types
 
 printer = DataPrinter(100);
+
+# Function needed to convert the dict, containing byte strings, to normal strings
+def convert(data):
+    try:
+        return data.decode()
+    except AttributeError:
+        pass
+
+    if isinstance(data, collections.Mapping):
+        return dict(map(convert, data.items()))
+    elif isinstance(data, collections.Iterable):
+        return type(data)(map(convert, data))
+    else:
+        return data
 
 def printAndParse(ans):
     if len(ans) > 0:
         ans = ans[0:-5]
         sys.stdout.write("\nAnswer: ")
 
-        for x in ans:
-            sys.stdout.write(x.encode("hex") + " ")
-
         parsed = msgpack.unpackb(ans)
-        print("")
-        print(parsed)
-        printer(parsed);
-
+        converted = convert(parsed);
+        json_data = json.dumps(converted);
+        print(json_data)
+        printer.plot_data(json_data);
 
 def main():
     serialFile = os.path.abspath(sys.argv[1])
     inputFile = os.path.abspath(sys.argv[2])
     outputFile = os.path.abspath(sys.argv[3])
-    
-    ser = serial.Serial(serialFile, 115200, timeout=0.5)
+
+    ser = serial.Serial(serialFile, 115200, timeout=1)
 
     while True:
         with open(inputFile, "rb") as f:
@@ -40,10 +53,10 @@ def main():
         open(inputFile, "wb")
         time.sleep(0.01)
 
-        answer = ser.read_until("\0\0\0\0\0")
+        answer = ser.read_until(b'\0\0\0\0\0')
         printAndParse(answer)
         with open(outputFile, "r+b") as o:
-            answer = ser.read_until("\0\0\0\0\0")
+            answer = ser.read_until(b'\0\0\0\0\0')
             o.write(answer)
             printAndParse(answer)
 
