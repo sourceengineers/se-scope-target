f70561d45cb1888f0aa5953310c6dd4285e86b52
rockresu <schuepbs@gmail.com>
2019-04-24 13:44:32 +0200

    Removed dead code

--- include/Scope/Scope/GeneralPurpose/BufferedIntStream.h
+++ include/Scope/Scope/GeneralPurpose/BufferedIntStream.h
@@ -44,56 +44,6 @@ BufferedIntStreamHandle BufferedIntStream_create(size_t capacity);
 IIntStreamHandle BufferedIntStream_getIIntStream(BufferedIntStreamHandle self);
 
 /**
- * Flushes the stream
- * @param self
- */
-void BufferedIntStream_flush(BufferedIntStreamHandle self);
-
-/**
- * Returns the amount of data currently pending in the stream
- * @param self
- * @return
- */
-size_t BufferedIntStream_length(BufferedIntStreamHandle self);
-
-/**
- * Writes "length" amount of data into the stream
- * @param self
- * @param data Data from which the data has to be written
- * @param length Amount of data that has to be written
- */
-void BufferedIntStream_write(BufferedIntStreamHandle self, const uint32_t* data, const size_t length);
-
-/**
- * Writes one single data point into the stream
- * @param self
- * @param data
- */
-void BufferedIntStream_writeInt(BufferedIntStreamHandle self, const uint32_t data);
-
-/**
- * Reads length amount of data from the stream and writes it into data
- * @param self
- * @param data Array into which the data gets written
- * @param length Size of array. If this is smaller than the amount of data pending, the function gets rejected.
- */
-void IntStream_read(BufferedIntStreamHandle self, uint32_t* data, const size_t length);
-
-/**
- * Returns one single data point
- * @param self
- * @return
- */
-uint32_t IntStream_readInt(BufferedIntStreamHandle self);
-
-/**
- * Returns of more data is pending or not
- * @param self
- * @return True if data pending. False if not
- */
-bool IntStream_byteIsReady(BufferedIntStreamHandle self);
-
-/**
  * Deconstructor
  * @param self
  */
