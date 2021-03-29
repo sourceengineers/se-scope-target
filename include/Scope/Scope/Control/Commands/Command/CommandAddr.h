7d7728d6b6bd6bf0a7d1537fa4cbca3b3876426a
Samuel Schuepbach <samuel.schuepbach@sourceengineers.com>
2021-03-29 17:03:47 +0200

    Added license headers

--- include/Scope/Scope/Control/Commands/Command/CommandAddr.h
+++ include/Scope/Scope/Control/Commands/Command/CommandAddr.h
@@ -3,6 +3,20 @@
  *
  * @copyright    Copyright (c) 2018 by Sourceengineers. All Rights Reserved.
  *
+ * @license      This file is part of SE-Scope.
+ *
+ *               SE-Scope is free software; you can redistribute it and/or
+ *               modify it under the terms of the GNU General Public License as
+ *               published by the Free Software Foundation.
+ *
+ *               SE-Scope is distributed in the hope that it will be useful,
+ *               but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *               MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *               GNU General Public License for more details.
+ *
+ *               You should have received a copy of the GNU General Public License
+ *               along with SE-Scope.  If not, see <http://www.gnu.org/licenses/>.
+ *
  * @authors      Samuel Schuepbach samuel.schuepbach@sourceengineers.com
  *
  * @brief        Implementation of the cf_addr command
--- public/Scope/GeneralPurpose/DataTypes.h
+++ public/Scope/GeneralPurpose/DataTypes.h
@@ -3,9 +3,19 @@
  *
  * @copyright    Copyright (c) 2018 by Sourceengineers. All Rights Reserved.
  *
- * @license This file is part of "se-scope-target". It is release under a commercial 
- *          license. Refer to the license PDF file received together with this code 
- *          for the exact licensing terms. 
+ * @license      This file is part of SE-Scope.
+ *
+ *               SE-Scope is free software; you can redistribute it and/or
+ *               modify it under the terms of the GNU General Public License as
+ *               published by the Free Software Foundation.
+ *
+ *               SE-Scope is distributed in the hope that it will be useful,
+ *               but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *               MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *               GNU General Public License for more details.
+ *
+ *               You should have received a copy of the GNU General Public License
+ *               along with SE-Scope.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @authors      Samuel Schuepbach samuel.schuepbach@sourceengineers.com
  *
