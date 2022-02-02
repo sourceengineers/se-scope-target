65216a7ee6aa62cd7a18dbbfa2dc50a5eb9d3e3a
Samuel Schupebach <samuel.schuepbach@sourceengineers.com>
2022-02-02 09:42:21 +0100

    Improved toolchain file to remove warnings

--- tolchains/ti-c2000.cmake
+++ tolchains/ti-c2000.cmake
@@ -2,11 +2,16 @@
 #
 #     set(CG_TOOL_ROOT        "/opt/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS")
 #     set(DEVICE_SUPPORT_ROOT "/home/asdf/controlSUITE/device_support/F2807x/v210")
-#
+#     Or set them like in the example below
 # Then do:
 #     mkdir build && cd build
 #     cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain-ti-c2000.cmake ..
 #     make
+# The ti example in the example folder has been compiled with:
+#     cmake -DCMAKE_TOOLCHAIN_FILE=../tolchains/ti-c2000.cmake \
+#           -DCG_TOOL_ROOT=/opt/ccstudio/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS \
+#           -DDEVICE_SUPPORT_ROOT=/opt/ti/c2000/C2000Ware_3_04_00_00/device_support/f2806x \
+#           -DGLOBAL_COMPILER_FLAGS="-v28 -ml -mt --cla_support=cla0 --float_support=fpu32 --vcu_support=vcu0 --advice:performance=all  -g --c11 --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --define=uint8_t=uint16_t --define=int8_t=int16_t"  ..
 
 if(NOT DEFINED CG_TOOL_ROOT)
     message(FATAL_ERROR "CG_TOOL_ROOT is not defined in options.cmake")
@@ -23,17 +28,31 @@ endif()
 if(NOT IS_DIRECTORY "${DEVICE_SUPPORT_ROOT}")
     message(FATAL_ERROR "The specified directory DEVICE_SUPPORT_ROOT does not exist: ${DEVICE_SUPPORT_ROOT}")
 endif()
-message(STATUS "Found a valid options.cmake")
 
-INCLUDE(CMakeForceCompiler)
-# this one is important
-SET(CMAKE_SYSTEM_NAME Generic)
-#this one not so much
-SET(CMAKE_SYSTEM_VERSION 1)
+# set target system
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_VERSION  1)
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-# specify the cross compiler
-CMAKE_FORCE_C_COMPILER(${CG_TOOL_ROOT}/bin/cl2000 "TI")
-CMAKE_FORCE_CXX_COMPILER(${CG_TOOL_ROOT}/bin/cl2000 "TI")
+# toolchain paths
+find_program(TI_GCC             NAMES   cl2000    PATHS  ${CG_TOOL_ROOT}/bin    NO_DEFAULT_PATH)
+find_program(TI_CXX             NAMES   cl2000    PATHS  ${CG_TOOL_ROOT}/bin    NO_DEFAULT_PATH)
+find_program(TI_AS              NAMES   cl2000    PATHS  ${CG_TOOL_ROOT}/bin    NO_DEFAULT_PATH)
+find_program(TI_AR              NAMES   ar2000    PATHS  ${CG_TOOL_ROOT}/bin    NO_DEFAULT_PATH)
+find_program(TI_OBJCOPY         NAMES   ofd2000   PATHS  ${CG_TOOL_ROOT}/bin    NO_DEFAULT_PATH)
+find_program(TI_OBJDUMP         NAMES   hex2000   PATHS  ${CG_TOOL_ROOT}/bin    NO_DEFAULT_PATH)
+find_program(TI_SIZE            NAMES   size2000  PATHS  ${CG_TOOL_ROOT}/bin    NO_DEFAULT_PATH)
+find_program(TI_LD              NAMES   cl2000    PATHS  ${CG_TOOL_ROOT}/bin    NO_DEFAULT_PATH)
+
+# set executables settings
+set(CMAKE_C_COMPILER    ${TI_GCC})
+set(CMAKE_CXX_COMPILER  ${TI_CXX})
+set(AS                  ${TI_AS})
+set(AR                  ${TI_AR})
+set(OBJCOPY             ${TI_OBJCOPY})
+set(OBJDUMP             ${TI_OBJDUMP})
+set(SIZE                ${TI_SIZE})
+set(LD                  ${TI_LD})
 
 # Add the default include and lib directories for tool chain
 include_directories(
